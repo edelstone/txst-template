2643cf1e21734d39e107a7b99faa872a3d89e036
Michael Edelstone <michael.edelstone@gmail.com>
2015-09-21 16:29:15 -0500

    more animations

--- js/custom-10.js
+++ js/custom-10.js
@@ -3,7 +3,25 @@
 $(".accordion").accordion({
 	collapsible: true,
 	active: false,
+	animate: 300,
+	heightStyle: "content",
+	alwaysOpen: false,
+	active: false
+});
+
+$(".accordion-2").accordion({
+	collapsible: true,
+	active: false,
 	animate: 150,
+	heightStyle: "content",
+	alwaysOpen: false,
+	active: false
+});
+
+$(".accordion-3").accordion({
+	collapsible: true,
+	active: false,
+	animate: false,
 	heightStyle: "content",
 	alwaysOpen: false,
 	active: false
